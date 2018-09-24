6fdc6fa87b8eb98da40f9f50a1b67ec7eaa3cfec
kazuya <b1700260@planet.kanazawa-it.ac.jp>
2018-09-24 17:48:28 +0900

    first commit

---
+++ pin_defination.h
@@ -0,0 +1,43 @@
+/* pins for LED */
+#define LED1 PE0
+#define LED2 PE1
+#define LED3 PE2
+#define LED4 PE3
+#define LED5 PE8
+#define LED6 PE9
+#define LED7 PE11
+#define LED8 PE13
+#define RESET PE4
+//////////////////
+
+/* pins for SW */
+#define SW1 PA4
+#define SW3 PA7
+#define SW4 PB0
+#define SW5 PB1
+/////////////////
+
+/* pin for PWM */
+// for Timer1
+#define TIM1_CH1 PE9
+#define TIM1_CH2 PE11
+#define TIM1_CH3 PE13
+#define TIM1_CH4 PE14
+
+// for Timer2
+#define TIM2_CH1 PA5
+#define TIM2_CH2 PB3
+#define TIM2_CH3 PA2
+#define TIM2_CH4 PA3
+
+// for Timer8
+#define TIM8_CH1 PC6
+#define TIM8_CH2 PC7
+#define TIM8_CH3 PC8
+#define TIM8_CH4 PC9
+/////////////////////
+
+
+
+
+  
